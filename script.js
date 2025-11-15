3ff470aaae7d8374c35f872f59d3ec4f1a803db3
Davibzf <davibezerrafraga@gmail.com>
2025-11-15 12:23:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const carouselData = [
         caption: "Meu coração bate no ritmo do seu nome."
     },
     {
-        image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+        image: "https://imagizer.imageshack.com/img923/9226/gEPXeO.png",
         caption: "Ao seu lado, cada momento é especial."
     }
 ];
